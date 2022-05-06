61405abdc040edb8d529fbfc914eda325818c802
Javid146 <mammadli.javid15@gmail.com>
2022-05-06 13:57:13 -0400

    wdw

--- src/test/java/com/cbt/runners/BatchRunner.java
+++ src/test/java/com/cbt/runners/BatchRunner.java
@@ -1,15 +1,34 @@
 package com.cbt.runners;
 
 import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
+/*// we need extentreports, junit-jupiter-engine, junit-platform-runner dependencies to run tests with JUNIT
+//if you want to run tests with MAVEN add maven-surefire dependency and add name of this class in pom file, inside of that dependency like below:
+                               /*git <plugin>
+                                <groupId>org.apache.maven.plugins</groupId>
+                             <artifactId>maven-surefire-plugin</artifactId>
+                                <version>3.0.0-M4</version>
+                                <configuration>
+                                    <includes>
+                                        <include>**///RegressionRunner.java</include>
+                    //                    </includes>
+                  //                      <testFailureIgnore>false</testFailureIgnore>
+                 //                       </configuration>
+             //                           </plugin>
+
+
 @RunWith(JUnitPlatform.class)
-@SuiteDisplayName("Automated api tests")
-@SelectPackages("com.cbt.tests.day11_cookie_and_extent_report")
+@SuiteDisplayName("Automated api tests") //shows the name of suite (test in console) like that
+@SelectPackages("com.cbt.tests.day11_cookie_and_extent_report") //paste path of package you want to run
+//@SelectClasses() //todo you select separate classes to run to. But make sure classname has the word test in it, otherwise will not run.
 public class BatchRunner {
 }
 
 
-// @RunWith(JUnitPlatform.class) --> create a batch runner
+// @RunWith(JUnitPlatform.class) --> create a batch runner. It means we can use several packages or classes together.
+/*@RunWith(JUnitPlatform.class) is from JUNIT4 AND 5. As they don't have annotations like @BeforeAll, @AfterAll like TestNG
+* we need to do like that*/
--- src/test/java/com/cbt/utilities/ExtentConfig.java
+++ src/test/java/com/cbt/utilities/ExtentConfig.java
@@ -5,30 +5,33 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.jupiter.api.extension.*;
 
+/*THIS CLASS SHOWS THE WAY OF CREATING REPORT VIA USING JUNIT5. So we implement 4 INTERFACES as below.
+* and Override them. These interfaces are like @BeforeSuite, @BeforeClass annotations of TestNG, but they are from JUNIT5.
+* They will trigger and compile report after testing is done.
+* todo In order to use this reporting all we need to do is to add @ExtendWith(ExtentConfig.class), on top of the class we want to make report from.*/
+
+
+
 public class ExtentConfig implements BeforeAllCallback, BeforeTestExecutionCallback, AfterAllCallback, AfterTestExecutionCallback{
     static ExtentReports reports;
     public static ExtentTest test;
 
-    // before each class
+    // todo before each class (BeforeAllCallback)
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
         // this line create a report under the test-output folder with the name of the class
         String filename = System.getProperty("user.dir") + "/test-output/" + context.getDisplayName() + "_Results.html";
         ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(filename);
         reports = new ExtentReports();
-        reports.attachReporter(htmlReporter);
+        reports.attachReporter(htmlReporter); }
 
-    }
-
-    // before each test in that class
+    // todo before each test in that class (BeforeTestExecutionCallback)
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
         test = reports.createTest(context.getDisplayName());
-        test.log(Status.INFO, context.getDisplayName() + " - started");
+        test.log(Status.INFO, context.getDisplayName() + " - started"); }
 
-    }
-
-    // after each test in that class
+    // todo after each test in that class (AfterTestExecutionCallback)
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception {
         if (!context.getExecutionException().isPresent()) {
@@ -38,11 +41,9 @@ public class ExtentConfig implements BeforeAllCallback, BeforeTestExecutionCallb
             test.fail(context.getExecutionException().get().getCause());
         } else {
             test.fatal(context.getExecutionException().get().toString());
-            test.fatal(context.getExecutionException().get().getCause());
-        }
-    }
+            test.fatal(context.getExecutionException().get().getCause()); }}
 
-    // after each class
+    // todo after each class (AfterAllCallback)
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
         reports.flush();
